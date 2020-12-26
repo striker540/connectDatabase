cd31420b98edfb0bae7f36e999941cb6cac92c71
Suresh Donepudi <donepudisuresh540@gmail.com>
2020-12-26 11:11:57 +0530

    First commit

---
+++ src/main/java/com/example/connectcompany/ConnectcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectcompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectcompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connectcompany/ConnectcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connectcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectcompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
